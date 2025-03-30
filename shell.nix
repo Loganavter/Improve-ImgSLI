b38c56aa00feb9b4fcf21e3bbec6795a3a273a90
BuyMyMojo <hello+git@buymymojo.net>
2025-03-31 02:25:25 +1100

    Add shell.nix for use with nix

---
+++ shell.nix
@@ -0,0 +1,23 @@
+let
+  # We pin to a specific nixpkgs commit for reproducibility.
+  # Last updated: 2025-03-31 15:00UTC.
+  pkgs =
+    import
+      (fetchTarball "https://github.com/NixOS/nixpkgs/archive/eb0e0f21f15c559d2ac7633dc81d079d1caf5f5f.tar.gz")
+      { };
+in
+pkgs.mkShell {
+  packages = [
+    (pkgs.python313.withPackages (
+      python-pkgs: with python-pkgs; [
+        # select Python packages here
+        mpmath
+        networkx
+        numpy
+        pillow
+        pyqt6
+        pyqt6-sip
+      ]
+    ))
+  ];
+}
