497e7fb5f5570b8130efe6fbcf0cc04f5b796eef
Loganavter <66126148+Loganavter@users.noreply.github.com>
2025-04-05 02:36:49 +0300

    Delete Improve_ImgSLI.py

--- Improve_ImgSLI.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt6.QtWidgets import QApplication
-from image_comparison_app import ImageComparisonApp
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = ImageComparisonApp()
-    ex.show()
-    sys.exit(app.exec())
