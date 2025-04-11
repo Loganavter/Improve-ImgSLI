3ce4eb123caab0683a5cc32d243dac053767e590
Loganavter <66126148+Loganavter@users.noreply.github.com>
2025-04-12 01:35:09 +0300

    Update launcher.sh

--- build/launcher.sh
+++ build/launcher.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
-exec python "$SCRIPT_DIR/./src/Improve_ImgSLI.py" "$@"
+exec python /usr/lib/improve-imgsli/Improve_ImgSLI.py "$@"
